f73bd0c8076e04cc478c4f6971e59bf02edb2f6d
ian mccauley <ianjmccauley@gmail.com>
2025-07-29 22:32:21 -0500

    fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ function App() {
             width: "100%",
             height: "100%",
             border: "none",
-            position: "initial",
+            display: "block",
             // position: "relative",
           }}
         ></iframe>
