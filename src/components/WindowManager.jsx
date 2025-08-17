6ddb3ac7fa4c83c1084d00a83b0c5c67e17a1adf
ian mccauley <ianjmccauley@gmail.com>
2025-08-16 20:47:53 -0500

    switch statement for window manager

--- src/components/WindowManager.jsx
+++ src/components/WindowManager.jsx
@@ -14,42 +14,64 @@ function WindowManager() {
     if (openWindows) {
       // console.log(openWindows)
       return openWindows.map((file, index) => {
-        // return console.log(file)
-        if (file.display !== false && file.type !== "folder") {
-          return (
-            <WindowWrapper
-              title={file.name}
-              startHeight={file.startHeight}
-              startWidth={file.startWidth}
-              windowId={file.name}
-              close={closeWindow}
-              key={index}
-            >
-              <iframe
-                src={file.link}
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  border: "none",
-                  display: "block",
-                }}
-              ></iframe>
-            </WindowWrapper>
-          )
-        } else if (file.type === "folder") {
-          console.log(file)
-          return (
-            <WindowWrapper
-              title={file.name}
-              // startHeight={file.startHeight}
-              // startWidth={file.startWidth}
-              windowId={file.name}
-              close={closeWindow}
-              key={index}
-            >
-              <FileExplorer folder={file.link} />
-            </WindowWrapper>
-          )
+        if (file.display !== false) {
+          switch (file.type) {
+            case "folder":
+              return (
+                <WindowWrapper
+                  title={file.name}
+                  // startHeight={file.startHeight}
+                  // startWidth={file.startWidth}
+                  windowId={file.name}
+                  close={closeWindow}
+                  key={index}
+                >
+                  <FileExplorer folder={file.link} />
+                </WindowWrapper>
+              )
+            case "image":
+              return (
+                <WindowWrapper
+                  title={file.name}
+                  startHeight={file.startHeight}
+                  startWidth={file.startWidth}
+                  windowId={file.name}
+                  close={closeWindow}
+                  key={index}
+                >
+                  <img
+                    src={file.link}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      border: "none",
+                      display: "block",
+                    }}
+                  ></img>
+                </WindowWrapper>
+              )
+            default:
+              return (
+                <WindowWrapper
+                  title={file.name}
+                  startHeight={file.startHeight}
+                  startWidth={file.startWidth}
+                  windowId={file.name}
+                  close={closeWindow}
+                  key={index}
+                >
+                  <iframe
+                    src={file.link}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      border: "none",
+                      display: "block",
+                    }}
+                  ></iframe>
+                </WindowWrapper>
+              )
+          }
         }
       })
     }
