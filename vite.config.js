d306a8405fd33dd9cd097c32a70fb716b46202ff
ian mccauley <ianjmccauley@gmail.com>
2025-09-21 16:06:11 -0500

    attempting to fix some config stuff

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	build: {
-		sourcemap: false,
-	},
-	server: {
-		port: 9000, // Replace 3001 with your desired port
-	},
-});
+  plugins: [react(), tailwindcss()],
+  base: "/portfolio-site/",
+  build: {
+    sourcemap: false,
+  },
+  server: {
+    port: 9000, // Replace 3001 with your desired port
+  },
+})
